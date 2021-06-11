76dfaf1f04abb292869a0cdf57c7f476bc587ded
Manuel Bonini <manuelbonini17@gmail.com>
2021-06-11 17:09:39 +0200

    first commit

---
+++ database/migrations/2021_06_11_144822_create_packages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('city', 30);
+            $table->string('country', 50);
+            $table->smallinteger('travel_days');
+            $table->float('price', 6, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
